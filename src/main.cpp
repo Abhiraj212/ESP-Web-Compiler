9ee8d02f874c084549b24348d98602b2b0ed68a9
Abhiraj <skatoch829@gmail.com>
2026-09-17 20:10:33 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #include <DHT.h>
 
 const char* WIFI_SSID     = "HOME2";
-const char* WIFI_PASSWORD = "<CHANGE_THIS_PASSWORD>";
+const char* WIFI_PASSWORD = "09876532";
 
 #define LCD_I2C_ADDRESS 0x27
 LiquidCrystal_I2C lcd(LCD_I2C_ADDRESS, 16, 2);
